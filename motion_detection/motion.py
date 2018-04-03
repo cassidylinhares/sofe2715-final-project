c6e684867ed424ad683387515cab0ec0772d3316
Ethan Elliott <ethan.elliott@hotmail.ca>
2018-04-03 01:18:27 -0400

    galton board complete

--- motion_detection/motion.py
+++ motion_detection/motion.py
@@ -6,50 +6,58 @@ import pygame.camera
 import numpy as N
 import pygame.surfarray
 
-running  = True
+running = True
 
 previousFrame = None
 isFirstFrame = True
 
 win = {
-    "width":640,
-    "height":480
+    "width": 640,
+    "height": 480
 }
 
+
 def luminenceCalc(r, g, b):
-    return  0.3 * r + 0.59 * g + 0.11 * b
+    return 0.3 * r + 0.59 * g + 0.11 * b
+
 
 def convertToGrayscale(surfArr):
     weights = N.array([0.3, 0.59, 0.11])
-    surfArr[:,:,0] = luminenceCalc(surfArr[:,:,0], surfArr[:,:,1], surfArr[:,:,2])
-    surfArr[:,:,1] = luminenceCalc(surfArr[:,:,0], surfArr[:,:,1], surfArr[:,:,2])
-    surfArr[:,:,2] = luminenceCalc(surfArr[:,:,0], surfArr[:,:,1], surfArr[:,:,2])
+    surfArr[:, :, 0] = luminenceCalc(
+        surfArr[:, :, 0], surfArr[:, :, 1], surfArr[:, :, 2])
+    surfArr[:, :, 1] = luminenceCalc(
+        surfArr[:, :, 0], surfArr[:, :, 1], surfArr[:, :, 2])
+    surfArr[:, :, 2] = luminenceCalc(
+        surfArr[:, :, 0], surfArr[:, :, 1], surfArr[:, :, 2])
     return surfArr.astype(N.int8)
 
-#start pygame
+
+# start pygame
 pygame.init()
-#start camera
+# start camera
 pygame.camera.init()
-#load list of cameras available
+# load list of cameras available
 cameras = pygame.camera.list_cameras()
-#select the first one because it is most likely the correct one
+# select the first one because it is most likely the correct one
 cam = pygame.camera.Camera(cameras[0])
-#start the camera, begin image stream
+# start the camera, begin image stream
 cam.start()
 
-#create window with defined width/height
-screen = pygame.display.set_mode((win['width'],win['height']))
+# create window with defined width/height
+screen = pygame.display.set_mode((win['width'], win['height']))
 # add a title to make it look pretty
 pygame.display.set_caption("SOFE2715 - Motion Detection")
 
-#begin main loop
+# begin main loop
 while running:
     for event in pygame.event.get():
-		if event.type == pygame.QUIT: running = False
+        if event.type == pygame.QUIT:
+            running = False
     currentFrame = N.array(pygame.surfarray.array3d(cam.get_image()))
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
     if (not isFirstFrame):
-        frameDiff = abs(convertToGrayscale(previousFrame) - convertToGrayscale(currentFrame))
+        frameDiff = abs(convertToGrayscale(previousFrame) -
+                        convertToGrayscale(currentFrame))
         arrMap = N.where(frameDiff > 150)
         print(len(arrMap))
         pygame.surfarray.blit_array(screen, frameDiff)
