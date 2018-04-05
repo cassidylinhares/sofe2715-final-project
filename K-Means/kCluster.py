4cd46266cd8e2e0699a3d83362552b42f437f980
Cassidy Linhares <100615025@oncampus.local>
2018-04-05 09:20:44 -0400

    fixed graphing

--- K-Means/kCluster.py
+++ K-Means/kCluster.py
@@ -1,31 +1,37 @@
 import csv
 import random
 import math
-import plotly.plotly as py
-import plotly.graph_objs as go
-#plotly to plot graph
+import pygame
+import time
 
+
+#starting state for window
+running = True
+#window size
+win = {"width": 800, "height": 800}
+
+#global variables
 COORDINATES = []
 CENTROID = []
 clusters = []
 
 K = 3
 
-def readIntoFile(filename):     #read csv file and store into coordinates array
+XMIN = 10000
+YMIN = 10000
+XMAX = -10000
+YMAX = -10000
+
+def readIntoFile(filename):  # read csv file and store into coordinates array
     with open(filename, "r") as f:
         reader = csv.reader(f)
         for line in reader:
             #find max and min x and y values to generate initial values
             COORDINATES.append([float(line[0]),float(line[1])]) #COORDINATES[[x,y]]
-    for point in COORDINATES:
-        print("Points: " + str(point))
 
-def generateInitialK(): #generate N number of centroids using min & max as bounds
-    #minX = minY = 10000
-    #maxX = maxY = -10000
-    XMIN = YMIN = 10000
-    XMAX = YMAX = -10000
-    for pt in COORDINATES:
+def generateInitialK(): # generate N number of centroids using min & max as bounds
+    global XMIN, XMAX, YMIN, YMAX
+    for pt in COORDINATES: #find max and min
         if pt[0] > XMAX:
             XMAX = pt[0]
         if pt[1] > YMAX:
@@ -39,7 +45,7 @@ def generateInitialK(): #generate N number of centroids using min & max as bound
     for center in range(K):
         CENTROID.append([random.uniform(XMIN,XMAX), random.uniform(YMIN,YMAX)]) #add to centroid array [[x,y]]
     for kPoint in CENTROID:
-        print("Centroid:"  + str(kPoint))
+        print("Initial Centroid:"  + str(kPoint))
 
 def euclideanDist(p1, p2): #calculate distance between 2 given points
     x = p1[0] - p2[0]
@@ -61,8 +67,7 @@ def assignCluster(): #assign each point to the proper cluster
             minDist.append([float(euclideanDist(CENTROID[i],point)), int(i)])
         minDist.sort(key=lambda pt: pt[0]) #sort by the distance
         clusters[minDist[0][1]].append(point) #put the point with smallest distance in proper cluster
-        #clusters[cluster0[[x,y],[x,y]], cluster1[[x,y],[x,y]]]
-    print("Cluster length: " + str(len(clusters)))
+        # clusters[# of centroids/cluster][# pts in cluster][x,y]
 
 def clusterAvg(step):
     sumX = 0
@@ -73,15 +78,15 @@ def clusterAvg(step):
         sumX = 0
         sumY = 0
         for ptInClust in range(len(clusters[eachClust])):
-            if(len(clusters[eachClust]) >1):
+            if(len(clusters[eachClust]) >1): #summation of pts in cluster
                 sumX += clusters[eachClust][ptInClust][0]
                 sumY += clusters[eachClust][ptInClust][1]
                 size = len(clusters[eachClust])
-            elif(len(clusters[eachClust]) == 1):
+            elif(len(clusters[eachClust]) == 1): # if one pt in cluster
                 sumX = clusters[eachClust][ptInClust][0]
                 sumY = clusters[eachClust][ptInClust][1]
                 size = len(clusters[eachClust])
-            elif(len(clusters[eachClust]) <= 0) :
+            elif(len(clusters[eachClust]) <= 0) : # if 0 pts in cluster
                 size = len(clusters[eachClust])+1
                 sumX = random.uniform(XMIN,XMAX)
                 sumY = random.uniform(YMIN,YMAX)
@@ -91,7 +96,7 @@ def clusterAvg(step):
     for center in CENTROID:
         print(center)
 
-def converge(file, threshHold):
+def converge(file, iteration):
     diffX = 0
     diffY = 0
     step = 0
@@ -101,87 +106,85 @@ def converge(file, threshHold):
     assignCluster()
     clusterAvg(step)
 
-    for i in range(step, len(CENTROID)): #get differences bewteen first generated centroid and centroid after avg is taken
-        for j in range(step):
-            diffX = CENTROID[i][0] - CENTROID[j][0]
-            diffY = CENTROID[i][1] - CENTROID[j][1]
-            shift.append([float(diffX), float(diffY)])
-            print("Difference1: " + str(diffX) + ", " + str(diffY))
-
     i = 0
     #repeat assignment of points to clusters and averaging new centroids if difference between centroids is above acceptable limit
-    #or keep repeating until end of shift array. Shift of 0 means that there is no shift in centroids
-    while(i <= 15):#len(shift) and (abs(shift[i][0]) > threshHold and abs(shift[i][1]) > threshHold)):
+    #or keep repeating until number of iterations is complete.
+    while(i <= iteration):
         i +=1
-        step += K #to know which clusters to check avg of. Get avg of last K clusters
+        step += K #to know which clusters to check avg of. iterate over last K clusters
         assignCluster()
         clusterAvg(step)
-        '''
-        for i in range(step, len(CENTROID)): #recalculate difference for most recent calculated centroids, and add them to shift array
-            for j in range(step):
-                diffX = CENTROID[i][0] - CENTROID[j][0]
-                diffY = CENTROID[i][1] - CENTROID[j][1]
-                shift.append([float(diffX), float(diffY)])
-                print("Difference: " + str(diffX) + ", " + str(diffY))'''
-    print("Indec " + str(i) + " Diff len: " + str(len(shift)))
+        print("Index: "+ str(i))
+    print("Step: "+ str(step))
 
+
+def scale(n, start1, stop1, start2, stop2):
+    #maps the points so that all ranges fit in the screen
+    return (n-start1)/ (stop1-start1)*(stop2-start2)+start2
 
 def graph(): #plot with plotly
-    cluster_plot = []
-    k_plot = []
-    trace = []
-    center_point = []
-    colours = ['rgb(0, 0, 255)', 'rgb(0, 128, 0)', 'rgb(255, 0, 0)', 'rgb(0, 255, 255)', 'rgb(255, 191, 0)']
+    global running;
+    global XMIN, XMAX, YMIN, YMAX
+    colours = [(255,0,0) , (0,255,0), (0,0,255), (255,0,255), (0,255,255), (230,230,0)]
 
-    for pts in range(len(CENTROID)-1, len(CENTROID)-(1+K),-1): #grab the most recent centroids
-        k_plot.append(CENTROID[pts])
+    #start pygame
+    pygame.init()
 
-    for pts in range(len(clusters)-1, len(clusters)-(1+K),-1): #grab most recent cluster
-        cluster_plot.append(clusters[pts])
+    #set window size and title
+    screen = pygame.display.set_mode((win['width'], win['height']))
+    pygame.display.set_caption("K Mean Cluster")
 
-    for ln in range(len(cluster_plot)):
-        for pt in range(len(cluster_plot[ln])):
-            trace.append(go.Scattergl( #make a trace for each point cluster
-                x = cluster_plot[ln][pt][0],
-                y = cluster_plot[ln][pt][1],
-                name = 'Cluster ' + str(ln+1),
-                mode = 'markers',
-                marker = dict(
-                    size = 7,
-                    color = colours[ln],
-                    line = dict(
-                        width = 2,
-                        color = 'rgb(102, 102, 102)'
-                    )
-                )
-            ))
-        center_point.append(go.Scattergl( #make a trace for each centroid
-            x = k_plot[ln][0],
-            y = k_plot[ln][1],
-            name = 'Centroid ' + str(ln+1),
-            mode = 'markers',
-            marker = dict(
-                size = 12,
-                color = 'rgb(0, 0, 0)',
-                line = dict(
-                    width = 2,
-                )
-            )
-        ))
-    data = []
-    for i in trace:
-        data.append(i)
-    for i in center_point:
-        data.append(i)
-    layout = dict(title= 'Clusters', yaxis = dict(zeroline = False), xaxis = dict(zeroline = False))
-    fig = dict(data = data, layout = layout)
-    py.iplot(fig, filename= 'clusters') #plot data
+    while running:
+        #check if window is closed or quit button
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
 
-file = input("Enter filename: ")
-tolerance = input("Enter tolerance level: ")
-K_in = input("Enter number of clusters (must be between 2-5): ")
-while int(K_in) > 5 or int(K_in) < 2:
-    K_in = input("INVALID. Enter number of clusters (must be between 2-5): ")
+        #draw an image and invert colours
+        graph = pygame.Surface((win['width'], win['height']), pygame.SRCALPHA, 32)
+        graph = graph.convert_alpha()
+
+        # [# of centroids/cluster][# pts in cluster][x,y]
+        c = 0
+        for i in range(len(clusters)-1, len(clusters)-(1+K),-1):
+            for j in range(0,len(clusters[i])):
+                # scale points in cluster
+                pt = (int(scale(clusters[i][j][0], XMIN, XMAX, 0-5, win['width']-5)),
+                int(scale(clusters[i][j][1], YMIN, YMAX, 0+5, win['height']-5)))
+                # draw points in cluster
+                pygame.draw.circle(graph, colours[c], pt, 5, 0)
+            c += 1
+            # scale centroid
+            k_pt = (int(scale(CENTROID[i][0], XMIN, XMAX, 0+8, win['width']-8)),
+            int(scale(CENTROID[i][1], YMIN, YMAX, 0+8, win['height']-8)))
+            # draw centroid
+            pygame.draw.circle(graph, (0,0,0), k_pt, 8, 0)
+
+        # flip image vertically so it's the correct orientation
+        graph = pygame.transform.flip(graph, False, True)
+        # scale image down to 95%
+        graph = pygame.transform.scale(graph, (int(0.95*win['width']), int(0.95*win['height'])))
+        # draw image to another image (double buffering)
+        screen.blit(graph, (0,0))
+        #draw the buffered image to screen
+        pygame.display.flip()
+    pygame.quit()
+
+#user input for filename, iterations, no. clusters
+file = input("Enter filename (csv): ")
+iteration = input("Enter iterations: ")
+K_in = input("Enter number of clusters: ")
+while int(K_in) > 6 or int(K_in) < 2:
+    K_in = input("INVALID. Enter number of clusters (must be between 2-6): ")
 K = int(K_in)
-converge(str(file), float(tolerance))
+
+#timer for length of time it takes to calculate
+startTime = time.time()
+converge(str(file), int(iteration))
+endTime = time.time()
 graph()
+
+print("Num Points: " + str(len(COORDINATES)))
+print("Time Elapsed: " + str(endTime - startTime))
+#fin
